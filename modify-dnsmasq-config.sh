4102ff49e36544d2a73fe9169232bb3afda707f1
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2020-02-15 16:48:43 +0700

    Make the script modular

--- modify-dnsmasq-config.sh
+++ modify-dnsmasq-config.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Run as sudo
+
+apt -y update; apt -y install dnsmasq
 cat >> /etc/dnsmasq.conf << EOL
 server=1.1.1.1
 server=8.8.8.8
 address=/test/$IP_ADDR
 EOL
+
+echo "nameserver $IP_ADDR" >> /etc/resolv.conf
+service dnsmasq restart
